ad8e2363a8bca891b157d4c3098ef15432689968
datnau <thanhdat.nau@gmail.com>
2026-03-05 15:26:20 -0600

    Update

---
+++ Lab/lab 6/Lab6Ex12.java
@@ -0,0 +1,65 @@
+public class Lab6Ex12{
+  public static void main(String args[]) {
+    int[] list;
+
+    list = createList();
+    printList(list);
+    System.out.println("\nThe maximum value is: " + findMax(list));
+
+    printListBackwards(list);
+
+    System.out.println("\nEnd of processing.");
+  }
+
+  public static int[] createList() {
+    int[] list = { 20,10,50,60,70,40,30 };
+
+    return list;
+  }
+
+  public static int findMax(int[] values) {
+    return findMaxRecursive(values, 0);
+  }
+
+  public static int findMaxRecursive(int[] values, int position) {
+    int result = 0;
+
+    if (position == values.length - 1) {
+      // Do something here???
+      return values[position];
+    } else {
+        result = findMaxRecursive(values, position + 1);
+        // And maybe some kind of if statement here???
+        if(values[position] > result){
+          return values[position];
+        }
+    }
+    return result;
+  } 
+  public static void printList(int[] array) {
+    printArrayRecursively(array, 0);
+  }
+
+  public static void printArrayRecursively(int[] array, int pos) {
+    if (pos == array.length-1) {
+      System.out.println(array[pos]);
+    } else {
+      System.out.print(array[pos] + " ");
+      printArrayRecursively(array, pos+1);
+    }
+  }
+
+  public static void printListBackwards(int[] array){
+    printArrayBackwardsRecursively(array,array.length-1);
+  }
+
+  public static void printArrayBackwardsRecursively(int[] array, int pos){
+    if(pos == 0){
+      System.out.print(array[pos]);
+    }else{
+      System.out.print(array[pos] + " ");
+      printArrayBackwardsRecursively(array, pos - 1);
+    }
+
+  }
+}
---
+++ Lab/lab 6/Lab6Ex3.java
@@ -0,0 +1,25 @@
+public class Lab6Ex3 {
+  public static void main(String args[]) {
+    System.out.println("Is 81 a power of three? " + isPowerOfThree(81));
+    System.out.println("Is 30 a power of three? " + isPowerOfThree(30));
+    System.out.println("Is 333 a power of three? " + isPowerOfThree(333));
+    System.out.println("Is 1 a power of three? " + isPowerOfThree(1));
+    System.out.println("Is 2657205 a power of three? " + isPowerOfThree(2657205));
+    System.out.println("Is 14348907 a power of three? " + isPowerOfThree(14348907));
+
+    System.out.println("\nEnd of processing.");
+  }
+ 
+  public static boolean isPowerOfThree(int n) {
+    // Obviously this is not correct
+    if(n%3 == 0){
+        return isPowerOfThree(n/3);
+    }
+    if(n == 1){
+        return true;
+    }if(n <= 0){
+        return false;
+    }
+    return false;
+  }
+}
