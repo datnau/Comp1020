e8f16dd33614ee9dbebac94bde168ca95ddfb33f
datnau <115642385+datnau@users.noreply.github.com>
2026-03-08 00:36:37 -0600

    update

--- Assignment 3/A3Phase1.java
+++ Assignment 3/A3Phase1.java
@@ -12,13 +12,44 @@ public class A3Phase1{
         return  result;
     }
     public static void printAAN(String[] adjectives, String[] nouns){
-
+        if(adjectives == null || nouns == null){
+            return;
+        }
+        printAANRecursively(adjectives, nouns, 0, 0, 0);
     }
 
-    public static
+    public static void printAANRecursively(String[] adj, String[] n, int i, int j, int k){
+        if(i >= adj.length){
+            return;
+        }
+        if(j >= adj.length){
+            printAANRecursively(adj, n, i+1, 0, 0);
+            return;
+        }
+        if(i==j){
+            printAANRecursively(adj, n, i, j+1, 0);
+            return;
+        }
+        if(k >= n.length){
+            printAANRecursively(adj, n, i, j+1, 0);
+            return;
+        }
+        System.out.println(adj[i] + " " + adj[j] + " " + n[k]);
+        printAANRecursively(adj, n, i, j, k + 1);
+
+
+    }
 
     public static void main(String[] args) {
         String result = rulerSequence(8);
         System.out.println(result);
+
+        String[] adjective = { "hypothetical", "spherical" };
+        String[] nouns = { "universe", "cow" };
+
+        printAAN(adjective, nouns);
+
+
+
     }
 }
